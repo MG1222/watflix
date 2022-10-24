451a4277420be9abb85a475fc073d989c29695e8
Margad Gantuya <majagantuya@gmail.com>
2022-10-24 17:33:21 +0200

    conflict ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-<<<<<<< HEAD
+
 
 import React from 'react';
-=======
+
 import logo from './logo.svg';
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
->>>>>>> dev
+
 
 function App() {
 const navLinks = [
@@ -16,16 +16,13 @@ const navLinks = [
 
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-          <p>Hello world</p>
-      </header>
 
 
 
-=======
+
+
       <Navbar links={navLinks} brand="Watflix" />
->>>>>>> dev
+
     </div>
 
   );
