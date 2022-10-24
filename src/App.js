5c066dd90b2ab95c47236996a16fae74c01cbe40
Margad Gantuya <majagantuya@gmail.com>
2022-10-24 15:24:11 +0200

    install axios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import MovieApi from "./api/movieApi";
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <p>Hello world</p>
       </header>
+
+        <MovieApi />
+
     </div>
+
   );
 }
 
