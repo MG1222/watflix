a40d480c9592d30187cc9af8620147d7e80cb742
Agathe Chesneau <chesneau.agathe@gmail.com>
2022-10-25 14:15:50 +0200

    recherche debut

---
+++ src/Search.js
@@ -0,0 +1,35 @@
+import React, {useEffect, useState} from 'react';
+import Movie from "./Movie";
+import {fakeData} from "./fakeData";
+
+const Search = () => {
+    const [inputValue, setInputValue] = useState('');
+    const [loaded, setLoaded] = useState(false);
+    const [resultats, setResultats] = useState([]);
+
+
+    useEffect(() => {
+        if (inputValue !== '') {
+            setResultats(fakeData.results);
+            setLoaded(true);
+        }
+        else {
+              setLoaded(false);
+        }
+    }, [inputValue])
+
+    return (
+        <>
+            <input type='text' onChange={e => setInputValue(encodeURIComponent(e.target.value))}/>
+            <div className='resultats'>
+                {loaded && resultats.map((e, i) => {
+                    if (e.media_type==='movie') {
+                        return <Movie key={i} movie_id={e.id} />
+                    }
+                })}
+            </div>
+        </>
+    )
+}
+
+export default Search;
